01114d43045f62b25cfc59ecfac09f2745426f52
shibata_yuki <shibata_yuki@ogix.co.jp>
2025-01-13 19:09:06 +0900

    prefabによる更新を保存

--- Assets/Script/Behavior/EnemyBlackboardConstants.cs
+++ Assets/Script/Behavior/EnemyBlackboardConstants.cs
@@ -2,8 +2,14 @@ public static class EnemyBlackboardConstants
 {
     public const string Self = "Self";
     public const string Character = "Character";
-    public const string Move = "Move";
     public const string WayPoints = "WayPoints";
-    public const string FindTagName = "FindTagName";
     public const string Speed = "Speed";
+    public const string Distance = "Distance";
+    public const string CurrentMode = "CurrentMode";
+    public const string AttackName = "AttackName";
+    public const string Anim = "Anim";
+    public const string searcher = "searcher";
+    public const string DefaultAnimation = "DefaultAnimation";
+    public const string AttackDistanceThreshold = "AttackDistanceThreshold";
+    public const string ChaseDistanceThreshold = "ChaseDistanceThreshold";
 }
--- Assets/Script/Behavior/EnemyController.cs
+++ Assets/Script/Behavior/EnemyController.cs
@@ -5,40 +5,16 @@ public class EnemyController : MonoBehaviour
 {
     [SerializeField]
     private BehaviorGraphAgent agent;
-    [SerializeField]
-    private bool isMove = true;
-    [SerializeField]
-    private float moveSpeed = 2.0f;
 
     void Awake()
     {
         // 初期化の確認
-        bool move;
+        EnemyMode mode;
         float speed;
-        agent.BlackboardReference.GetVariableValue(EnemyBlackboardConstants.Move, out move);
+        agent.BlackboardReference.GetVariableValue(EnemyBlackboardConstants.CurrentMode, out mode);
         agent.BlackboardReference.GetVariableValue(EnemyBlackboardConstants.Speed, out speed);
 
-        Debug.Log($"speed:{speed} move:{move}");
-
-        agent.BlackboardReference.SetVariableValue(EnemyBlackboardConstants.Move, isMove);
-        agent.BlackboardReference.SetVariableValue(EnemyBlackboardConstants.Speed, moveSpeed);
-
-        agent.BlackboardReference.GetVariableValue(EnemyBlackboardConstants.Move, out move);
-        agent.BlackboardReference.GetVariableValue(EnemyBlackboardConstants.Speed, out speed);
-        Debug.Log($"speed:{speed} move:{move}");
+        Debug.Log($"speed:{speed} mode:{mode}");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //bool move;
-        //float speed;
-
-        //agent.BlackboardReference.GetVariableValue(EnemyBlackboardConstants.Move, out move);
-        //agent.BlackboardReference.GetVariableValue(EnemyBlackboardConstants.Speed, out speed);
-        //Debug.Log($"speed:{speed} move:{move}");
-
-        agent.BlackboardReference.SetVariableValue(EnemyBlackboardConstants.Move, isMove);
-        agent.BlackboardReference.SetVariableValue(EnemyBlackboardConstants.Speed, moveSpeed);
-    }
 }
