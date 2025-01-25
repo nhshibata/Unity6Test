b37d6118c890d1bcd36c2b84c8e4a42ba8263bad
shibata_yuki <shibata_yuki@ogix.co.jp>
2025-01-25 14:30:52 +0900

    ADV用のSceneとフォルダを作成

---
+++ Assets/Script/Common/UI/AspectKeeper.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+[ExecuteAlways]
+public class AspectKeeper : MonoBehaviour
+{
+    [SerializeField]
+    private Camera targetCamera;
+
+    [SerializeField]
+    private Vector2 aspectVec;
+
+    void Update()
+    {
+        var screenAspect = Screen.width / (float)Screen.height;
+        var targetAspect = aspectVec.x / aspectVec.y;
+
+        var magRate = targetAspect / screenAspect;
+
+        var viewportRect = new Rect(0, 0, 1, 1);
+
+        if (magRate < 1)
+        {
+            viewportRect.width = magRate;
+            viewportRect.x = 0.5f - viewportRect.width * 0.5f;
+        }
+        else
+        {
+            viewportRect.height = 1 / magRate;
+            viewportRect.y = 0.5f - viewportRect.height * 0.5f;
+        }
+
+        targetCamera.rect = viewportRect;
+    }
+}
